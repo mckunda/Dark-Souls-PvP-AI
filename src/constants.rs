acaf8bf87aea920e95d5167df916b24d16bdce40
mckunda <davenolt.sh@gmail.com>
2023-11-29 16:59:40 +0300

    refactor: move Player_loc_y_offsets_length to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -3,3 +3,4 @@ pub const PlayerId: i32 = 1;
 pub const Enemy_loc_x_offsets_length: usize = 4;
 pub const Player_loc_x_offsets_length: usize = 5;
 pub const Enemy_loc_y_offsets_length: usize = 5;
+pub const Player_loc_y_offsets_length: usize = 5;
