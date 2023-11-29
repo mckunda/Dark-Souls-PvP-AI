38163ef36e1fb08dce69193ffab3f7447fca49a3
mckunda <davenolt.sh@gmail.com>
2023-11-29 17:21:32 +0300

    refactor: move Enemy_Poise_offsets_length to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -42,3 +42,4 @@ pub const Player_Lock_on_offsets_length: usize = 5;
 pub const Player_twohanding_offsets_length: usize = 5;
 pub const Enemy_stamRecovery_offsets_length: usize = 5;
 pub const Player_Poise_offsets_length: usize = 5;
+pub const Enemy_Poise_offsets_length: usize = 5;
