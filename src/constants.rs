f5d34f7ff6348a3670e4e14a8b223e693b6047fe
mckunda <davenolt.sh@gmail.com>
2023-11-29 17:01:48 +0300

    refactor: move Enemy_animationType_offsets_length to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,4 @@ pub const Enemy_loc_y_offsets_length: usize = 5;
 pub const Player_loc_y_offsets_length: usize = 5;
 pub const Enemy_rotation_offsets_length: usize = 5;
 pub const Player_rotation_offsets_length: usize = 4;
+pub const Enemy_animationType_offsets_length: usize = 5;
